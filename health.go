670bc347ffb78a377905f270fea05ba40ca39a5a
Jordan Hasgul <jxh1243@student.bham.ac.uk>
2023-03-05 22:24:13 +0000

    added helper method to check a checker asynchronously

--- health.go
+++ health.go
@@ -1,10 +1,17 @@
 package health
 
-type Health string
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+type State string
 
 const (
-	Healthy   Health = "healthy"
-	Unhealthy Health = "unhealthy"
+	Healthy   State = "healthy"
+	Unhealthy State = "unhealthy"
 )
 
 type Checker interface {
@@ -15,4 +22,37 @@ type CheckerFunc func() error
 
 func (f CheckerFunc) Check() error {
 	return f()
+}
+
+func doCheck(checker Checker) error {
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+	defer cancel()
+
+	errs := make(chan error)
+	go func() {
+		defer func() {
+			recv := recover()
+			if recv != nil {
+				switch t := recv.(type) {
+				case fmt.Stringer:
+					errs <- fmt.Errorf("%s", t)
+				default:
+					panic(t)
+				}
+			}
+
+			close(errs)
+		}()
+
+		select {
+		case errs <- checker.Check():
+		case <-ctx.Done():
+			errs <- errors.New("timeout exceeded")
+		}
+	}()
+
+	return <-errs
 }
